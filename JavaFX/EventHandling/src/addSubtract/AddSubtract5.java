72cd4f348bd55d0468b18b077bf90bf078f4adf1
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-08-18 11:06:18 +0200

    Java Advanced

---
+++ JavaFX/EventHandling/src/addSubtract/AddSubtract5.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package addSubtract;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+
+/**
+ *
+ * @author Manex
+ */
+public class AddSubtract5 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+// Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(e -> btnAdd_Click());
+// Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction(e -> btnSubtract_Click());
+// Create the Label
+        lbl = new Label();
+        lbl.setText(Integer.toString(iCounter));
+// Add the buttons and label to an HBox pane
+        HBox pane = new HBox(10);
+        pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+// Add the layout pane to a scene
+        Scene scene = new Scene(pane, 200, 75);
+// Add the scene to the stage, set the title
+// and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Add/Sub");
+        primaryStage.show();
+    }
+
+    private void btnAdd_Click() {
+        iCounter++;
+        lbl.setText(Integer.toString(iCounter));
+    }
+
+    private void btnSubtract_Click() {
+        iCounter--;
+        lbl.setText(Integer.toString(iCounter));
+    }
+}
