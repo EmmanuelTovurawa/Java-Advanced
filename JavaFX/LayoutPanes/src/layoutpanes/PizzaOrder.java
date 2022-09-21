f4003d95f54c66831933aae5ace01426047355b5
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-09-21 14:46:36 +0200

    Java Advanced

--- JavaFX/LayoutPanes/src/layoutpanes/PizzaOrder.java
+++ JavaFX/LayoutPanes/src/layoutpanes/PizzaOrder.java
@@ -69,7 +69,7 @@ public class PizzaOrder extends Application {
         rdoLarge.setToggleGroup(groupSize);
         VBox paneSize = new VBox(lblSize, rdoSmall, rdoMedium, rdoLarge);
         paneSize.setSpacing(10);
-// Create the crust pane 
+// Create the crust pane with a label
         Label lblCrust = new Label("Crust");
         rdoThin = new RadioButton("Thin");
         rdoThick = new RadioButton("Thick");
