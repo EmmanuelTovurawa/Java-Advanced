f70df509990222927550dd5a69eb73ab4c32fc68
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-09-27 20:52:14 +0200

    Java Advanced

--- JavaFX/LayoutPanes/src/layoutpanes/PizzaOrder.java
+++ JavaFX/LayoutPanes/src/layoutpanes/PizzaOrder.java
@@ -87,7 +87,7 @@ public class PizzaOrder extends Application {
         VBox paneToppings = new VBox(lblToppings, chkPepperoni,
                 chkMushrooms, chkAnchovies);
         paneToppings.setSpacing(10);
-// Create the buttons ie the button
+// Create the buttons ie the buttons
         Button btnOK = new Button("OK");
         btnOK.setPrefWidth(80);
         btnOK.setOnAction(e -> btnOK_Click());
