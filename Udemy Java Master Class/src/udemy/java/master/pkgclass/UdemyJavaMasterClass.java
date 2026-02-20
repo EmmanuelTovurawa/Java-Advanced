015503108c82c096ea7bfac5d68b3eb4aeaf7b62
Emmanuel Tovurawa <emmanuel.tovurawa@gmail.com>
2026-02-20 10:12:55 +0200

    Java Advanced

---
+++ Udemy Java Master Class/src/udemy/java/master/pkgclass/UdemyJavaMasterClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package udemy.java.master.pkgclass;
+
+/**
+ *
+ * @author Manex
+ */
+public class UdemyJavaMasterClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
